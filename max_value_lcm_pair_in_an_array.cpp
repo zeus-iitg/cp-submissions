f1074bf200c5cae4eea167d146488af9d61ca6c5
zeus-iitg <42604439+zeus-iitg@users.noreply.github.com>
2020-04-29 06:07:38 +0530

    Create max_value_lcm_pair_in_an_array.cpp

---
+++ max_value_lcm_pair_in_an_array.cpp
@@ -0,0 +1,88 @@
+//Link to problem: https://www.codechef.com/problems/COW3G
+
+#include <bits/stdc++.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) ((a)*(b))/__gcd((a),(b))
+#define clr(x) x.clear()
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define mp make_pair
+#define hell 1000000007
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define all(a) a.begin(),a.end()
+#define ign cin.ignore();
+
+#define o_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define o_setll tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+//to delete an element
+//s.erase(s.find_by_order(s.order_of_key(ele)));
+//where s is the name of the ordered_set
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+using namespace std;
+using namespace __gnu_pbds;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef MY_PROJECT
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+int main(){
+    IOS
+    int n;
+    cin >> n;
+    vector<long long> arr(n);
+    for (auto &i: arr) cin>> i;
+    sort(arr.begin(), arr.end(), greater<>());
+    long long ans = 0;
+    bool found = false;
+    for (int i=0; i<n; i++)
+        for (int j=i+1;j<n && !found; j++){
+            if (arr[i]*arr[j]<ans) break;
+            long long g = __gcd(arr[i], arr[j]), temp = arr[i]*arr[j];
+            if (g==1 && temp>=ans){
+                ans = temp;
+                found = true;
+                break;
+            }
+            ans = max(ans, temp/ g);
+        }
+    cout << ans << endl;
+    return 0;
+}
