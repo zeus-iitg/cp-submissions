0c1c56cd1a410e1976c69800f5e857834ccb1285
Vaibhav Kumar Singh <vaibhavsingh878@gmail.com>
2021-01-19 22:13:55 +0530

    Update solution.cpp

--- dominator tree/solution.cpp
+++ dominator tree/solution.cpp
@@ -1,15 +1,15 @@
 //Link to problem: https://cses.fi/problemset/task/1703
 
 #include<bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 typedef pair<int,int>   II;
-typedef vector< II >      VII;
+typedef vector< II >    VII;
 typedef vector<int>     VI;
 typedef vector< VI > 	VVI;
 typedef long long int 	LL;
-
+ 
 #define PB push_back
 #define MP make_pair
 #define F first
@@ -17,15 +17,15 @@ typedef long long int 	LL;
 #define SZ(a) (int)(a.size())
 #define ALL(a) a.begin(),a.end()
 #define SET(a,b) memset(a,b,sizeof(a))
-
+ 
 #define si(n) scanf("%d",&n)
 #define dout(n) printf("%d\n",n)
 #define sll(n) scanf("%lld",&n)
 #define lldout(n) printf("%lld\n",n)
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
-
+ 
 #define TRACE
-
+ 
 #ifdef TRACE
 #define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
 template <typename Arg1>
@@ -39,7 +39,7 @@ void __f(const char* names, Arg1&& arg1, Args&&... args){
 #else
 #define trace(...)
 #endif
-
+ 
 //FILE *fin = freopen("in","r",stdin);
 //FILE *fout = freopen("out","w",stdout);
 const int N = int(1e5)+10;
@@ -91,14 +91,17 @@ int main()
 {
 	int n,m;
 	si(n);si(m);
+	int orig_n=n;
 	for(int i=0;i<m;i++)
 	{
 		int u,v;
 		si(u);si(v);
 		g[u].PB(v);
 	}
+ 
 	//Build Dominator tree
 	dfs0(1);
+ 
 	n=T;
 	for(int i=n;i>=1;i--)
 	{
@@ -121,13 +124,13 @@ int main()
 		tree[rev[i]].PB(rev[dom[i]]);
 		tree[rev[dom[i]]].PB(rev[i]);
 	}
-
+ 
 	int s=1; // source vertex
-
+ 
 	queue<int> q;
 	vector<bool> used(n+1);
 	vector<int> d(n+1), p(n+1);
-
+ 
 	q.push(s);
 	used[s] = true;
 	p[s] = -1;
@@ -143,16 +146,15 @@ int main()
 	        }
 	    }
 	}
-
-
+ 
     vector<int> path;
-    for (int v = n; v != -1; v = p[v])
+    for (int v = orig_n; v != -1; v = p[v])
         path.push_back(v);
     reverse(path.begin(), path.end());
     cout<<path.size()<<"\n";
     sort(path.begin(), path.end());
     for (int v : path)
         cout << v << " ";
-
+ 
 	return 0;
 }
